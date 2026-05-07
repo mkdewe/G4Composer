869cc6c24f6422930644fb9830c201c34c2adaaa
Relfekst <darekdziel@op.pl>
2026-05-07 12:14:05 +0200

    fix: sync EF Core model snapshot

---
+++ G4Composer.Server/Data/Migrations/20260507101358_SyncSnapshot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace G4Composer.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
